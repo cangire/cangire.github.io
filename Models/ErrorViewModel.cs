e1025e2f5796f4775311cb6cb67d32de680311eb
cangire <73399982+cangire@users.noreply.github.com>
2022-11-07 19:24:00 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cangire.github.io.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
